705294e20677f57d968feb0541030d608e4324cd
ychong05 <ychong05@students.cs.ubc.ca>
2024-03-02 10:50:45 -0800

    create player and board file

---
+++ player.py
@@ -0,0 +1,28 @@
+from board import Board
+import random
+
+class Player:
+    x = 1
+    y = 1
+    speed = 1
+    def move_right(self):
+        if self.x == Board.size:
+            self.x += self.speed
+        
+    def move_left(self):
+        if self.x > 1:
+            self.x -= self.speed
+            
+    def move_down(self):
+        if self.y == Board.size:
+            self.y += self.speed       
+    
+    def move_up(self):
+        if self.y > 1:
+            self.y -= self.speed
+            
+    def change_speed(self):
+        number = random.randint(-3, 3)
+        self.speed += number
+        if self.speed == 0:
+            self.speed = 1
