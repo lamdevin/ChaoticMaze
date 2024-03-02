cb34733792fb563f359af7f4657e60c57c19bb3f
Devin Lam <dla249@sfu.ca>
2024-03-02 10:49:55 -0800

    updated main

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
 import pygame
+import sys
+from pygame.locals import *
 
 pygame.init()
 
 SCREEN_WIDTH = 800
-SCREEN_HEIGHT = int(SCREEN_WIDTH * 0.8)
+SCREEN_HEIGHT = 800
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-pygame.display.set_caption('Demo')
+pygame.display.set_caption("ChaoticMaze")
 
-#set framerate
-clock = p
+FPS = pygame.time.Clock()
+FPS.tick(60)
 
+while True:
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
+    pygame.display.update()
