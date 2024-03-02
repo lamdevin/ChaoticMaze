e1cbdf0555a9a3a21e026c444d3be2103d4ebd8c
ychong05 <ychong05@students.cs.ubc.ca>
2024-03-02 11:18:22 -0800

    create board

--- board.py
+++ board.py
@@ -1,17 +1,35 @@
 import pygame
 
 class Board:
-    size = 12
+    def __init__(self):
+        self.size = 12
+        self.surface = pygame.display.set_mode((600, 600))
+        self.wall_color = (255, 0, 0)
+        self.goal_color = (255, 255, 0)
+        
+        self.board = [
+            [1, 1, 1, 1, 1, 1, 1, 1],
+            [1, 0, 0, 0, 1, 2, 0, 1],
+            [1, 0, 1, 0, 1, 1, 0, 1],
+            [1, 0, 1, 0, 0, 0, 0, 1],
+            [1, 0, 1, 0, 1, 0, 0, 1],
+            [1, 0, 1, 0, 1, 0, 0, 1],
+            [1, 0, 1, 0, 0, 0, 0, 1],
+            [1, 1, 1, 1, 1, 1, 1, 1]
+        ]
     
-    board = [
-        [1, 1, 1, 1, 1, 1, 1, 1],
-        [1, 0, 0, 0, 1, 2, 0, 1],
-        [1, 0, 1, 0, 1, 1, 0, 1],
-        [1, 0, 1, 0, 0, 0, 0, 1],
-        [1, 0, 1, 0, 1, 0, 0, 1],
-        [1, 0, 1, 0, 1, 0, 0, 1],
-        [1, 0, 1, 0, 0, 0, 0, 1],
-        [1, 1, 1, 1, 1, 1, 1, 1]
-    ]
-    
+    def draw(self):
+        pos_x = 0
+        pos_y = 0
+        rect_size = 60
+        for i in range(self.size):
+            for j in range(self.size):
+                if self.board[i][j] == 1:
+                    pygame.draw.rect(self.surface, self.wall_color, pygame.Rect(pos_x, pos_y, rect_size, rect_size))
+                elif self.board[i][j] == 2:
+                    pygame.draw.rect(self.surface, self.goal_color, pygame.Rect(pos_x, pos_y, rect_size, rect_size))
+                pos_x += rect_size
+                pos_y += rect_size
+            pos_x = pos_y = 0
+
     
