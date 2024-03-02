2ed4a2b0f7c5d6c0ccc67885bff185ebbb8bcd49
Devin Lam <dla249@sfu.ca>
2024-03-02 10:02:21 -0800

    updated main

---
+++ main.py
@@ -0,0 +1,13 @@
+import pygame
+
+pygame.init()
+
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = int(SCREEN_WIDTH * 0.8)
+
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption('Demo')
+
+#set framerate
+clock = p
+
